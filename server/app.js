cf549544f4b4a53786e9c1eb7495769f79dc2833
הימל צפרה <Hand4d10@mbjcomp.org.il>
2025-09-10 15:57:52 +0300

    fix: allow PATCH method in CORS for API

--- server/app.js
+++ server/app.js
@@ -58,7 +58,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // הוספתי PATCH
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 };
 
